686efdbaaa6334fd376ee0a13db24f38bf97eef5
sadman saqib <62344899+sadman9494@users.noreply.github.com>
2022-03-13 20:27:04 +0600

    new architecture

---
+++ Lakeshore Tourism/Home/view/home.php
@@ -0,0 +1,33 @@
+<html>
+    <body>
+     <table align = "center">    
+         <tr>
+          <td > <a href = "../../Admin/Admin_View/admin.php" > <h3>Admin </h3></a> </td>
+          <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+
+          <td > <a href = "../../Customer/Customer_View/customer.php"> <h3>Customer </h3></a> </td>
+          <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+
+          <td > <a href = "../../Hotel Owner/Hotel_View/hotel.php"> <h3>Hotel Owner </h3> </a> </td>
+          <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+          <td > <a href = "../../Local Transport Owner/Local_Transport_view/local.php"> <h3>Local Trasport Owner <h3></a> </td>
+          <br> <br> <br>
+        </tr>
+    </table>
+
+    <table>
+        <tr>
+            <td><h1> Our packages</h1></td>
+            
+        </tr>
+        <tr>
+        
+            <td><h1> Our Tours</h1></td>
+            
+        </tr>
+        <tr>
+            <td><h1> Our packages</h1></td>
+        </tr>
+    </table>
+    </body>
+</html>
